7129ea21e45c2f0d3fded07db2628c9707fb7fd6
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-05-20 20:46:40 +0300

    started writing code.

---
+++ src/main/java/com/collections1/aghw/hwcollections1/HwCollections1Application.java
@@ -0,0 +1,13 @@
+package com.collections1.aghw.hwcollections1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCollections1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwCollections1Application.class, args);
+	}
+
+}
