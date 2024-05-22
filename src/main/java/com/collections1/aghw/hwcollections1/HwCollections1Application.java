36d05a311c0a6ca26d07178e97efa18a42f66e55
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-05-22 15:50:20 +0300

    created an array of employees, wrote the code for the "Employee" class.

--- src/main/java/com/collections1/aghw/hwcollections1/HwCollections1Application.java
+++ src/main/java/com/collections1/aghw/hwcollections1/HwCollections1Application.java
@@ -8,6 +8,14 @@ public class HwCollections1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HwCollections1Application.class, args);
+		Employee[] employees = {
+				new Employee("Anna", "Yamada"),
+				new Employee("Marin", "Kitagava"),
+				new Employee("Mahiru", "Shiina")
+		};
+		for (int i = 0; i < employees.length; i++) {
+			System.out.println(employees[i]);
+		}
 	}
 
 }
