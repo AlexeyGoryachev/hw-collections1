78ec46cf47cdce53e998eb60eee7073fd37c7f9e
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-07-22 17:05:55 +0300

    created DepartmentNotFoundException updated EmployeeService

---
+++ src/main/java/com/collections1/aghw/hwcollections1/exception/DepartmentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.collections1.aghw.hwcollections1.exception;
+
+public class DepartmentNotFoundException extends RuntimeException {
+    public DepartmentNotFoundException(String message) {
+        super(message);
+    }
+}
